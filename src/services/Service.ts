b444a68e55042c6a910e3e0b1741c58edfd1d679
Edenilson Nascimento <67112084+Edenilson-Nascimento@users.noreply.github.com>
2025-06-09 11:40:18 -0300

    atualizacao service

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,40 +1,40 @@
 import axios from 'axios';
 
+// Exemplo: Substitua pela URL REAL que o Railway te der!
 export const api = axios.create({
-    baseURL: 'blogpessoal-production.up.railway.app'
+    baseURL: 'https://blogpessoal-production.up.railway.app'
 })
 
-    export const cadastroUsuario = async(url: any,dados: any,setDado: any) => { 
-        const resposta = await api.post(url,dados)
-        setDado(resposta.data)
-    }
+export const cadastroUsuario = async(url: string, dados: any, setDado: (data: any) => void) => {
+    const resposta = await api.post(url, dados);
+    setDado(resposta.data);
+}
 
-    export const login = async(url: any,dados: any,setDado: any) => { 
-        const resposta = await api.post(url,dados)
-        setDado(resposta.data.token)
-    }
+export const login = async(url: string, dados: any, setDado: (token: string) => void) => {
+    const resposta = await api.post(url, dados);
+    setDado(resposta.data.token);
+}
 
+export const busca = async(url: string, setDados: (data: any) => void, header: any) => {
+    const resposta = await api.get(url, header);
+    setDados(resposta.data);
+}
 
-    export const busca = async(url: any, setDados: any, header: any) => {
-        const resposta = await api.get(url, header)
-        setDados(resposta.data)
-    }
-    
-    export const buscaId = async(url: any, setDados: any, header: any) => {
-        const resposta = await api.get(url, header)
-        setDados(resposta.data)
-    }
-    
-    export const post = async(url: any, dados: any, setDados: any, header: any) => {
-        const resposta = await api.post(url, dados, header)
-        setDados(resposta.data)
-    }
-    
-    export const put = async(url: any, dados: any, setDados: any, header: any) => {
-        const resposta = await api.put(url, dados, header)
-        setDados(resposta.data)
-    }
-    
-    export const deleteId = async(url: any, header: any) => {
-        await api.delete(url, header)
-    }
+export const buscaId = async(url: string, setDados: (data: any) => void, header: any) => {
+    const resposta = await api.get(url, header);
+    setDados(resposta.data);
+}
+
+export const post = async(url: string, dados: any, setDados: (data: any) => void, header: any) => {
+    const resposta = await api.post(url, dados, header);
+    setDados(resposta.data);
+}
+
+export const put = async(url: string, dados: any, setDados: (data: any) => void, header: any) => {
+    const resposta = await api.put(url, dados, header);
+    setDados(resposta.data);
+}
+
+export const deleteId = async(url: string, header: any) => {
+    await api.delete(url, header);
+}
